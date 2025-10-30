712aca621b8bfccf43e4ca52a71880bf9f5aa091
Lucas Williams <surfluke19@gmail.com>
2025-10-29 22:18:19 -0700

    sync changes
    it *probably* works

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
-
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyADPvITfvuVYqQA5NWhdZeyw_fcA62rii4",
-    authDomain: "scouting-website-ee380.firebaseapp.com",
-    databaseURL: "https://scouting-website-ee380-default-rtdb.firebaseio.com",
-    projectId: "scouting-website-ee380",
-    storageBucket: "scouting-website-ee380.appspot.com",
-    messagingSenderId: "415208050900",
-    appId: "1:415208050900:web:674769095c12e3eaab6b41",
-    measurementId: "G-NR2HGV6G2G"
-  };
-  
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
+};
 
-    export const db = getDatabase(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
